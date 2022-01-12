325b40c619a5a7541988e9f3c4453f62b77533dc
Fube <fubeitch@gmail.com>
2022-01-11 23:32:58 -0500

    Added DaisyUI

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+// tailwind.config.cjs
+
+module.exports = {
+	// add this section
+	purge: ['./src/**/*.html', './src/**/*.svelte'],
+	darkMode: 'media', // or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: [require('daisyui')]
+};
