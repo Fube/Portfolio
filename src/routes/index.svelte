ced5761f03ff6eb3bb1ffd386e437d8ec711ab2d
Fube <fubeitch@gmail.com>
2022-01-13 15:33:07 -0500

    Added Parallax effect

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,45 @@
 <script lang="ts">
+	import { Parallax, ParallaxLayer } from 'svelte-parallax';
 </script>
 
-<div class="scroll-container" />
+<div class="flex flex-col h-screen">
+	<div class="mt-12 h-full">
+		<Parallax sections={3} disabled={false}>
+			<ParallaxLayer>
+				<section class="hero h-full text-left">
+					<div class="max-w-md">
+						<h2 class="mb-5 text-secondary text-sm">Hello there, I'm</h2>
+						<h1 class="mb-3 text-6xl font-bold ml-2">Fube</h1>
+						<h3 class="text-secondary">I'm a developer</h3>
+						[Arrow down here]
+					</div>
+				</section>
+			</ParallaxLayer>
 
-<div class="hero min-h-screen bg-base-200">
-	<div class="hero-content">
-		<div class="max-w-md">
-			<h1 class="mb-5 text-secondary text-sm">Hello there, I'm</h1>
-			<h2 class="mb-5 text-6xl font-bold">Fube</h2>
-		</div>
+			<ParallaxLayer offset={1}>
+				<section class="text-center">
+					<h1 class="text-primary text-4xl">Things I've done</h1>
+					<ul>
+						<li>Stuff</li>
+					</ul>
+				</section>
+			</ParallaxLayer>
+		</Parallax>
 	</div>
 </div>
+
+<style lang="postcss">
+	@counter-style thumbs {
+		system: cyclic;
+		symbols: '>';
+		suffix: ' ';
+	}
+
+	ul {
+		list-style: thumbs;
+	}
+
+	li {
+		@apply text-red-600;
+	}
+</style>
