d835f8225fb710427f2f7c0da04fff2e6163348d
Fube <fubeitch@gmail.com>
2022-01-12 22:22:56 -0500

    Added hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,13 @@
-<h1>Hello World</h1>
+<script lang="ts">
+</script>
+
+<div class="scroll-container" />
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-secondary text-sm">Hello there, I'm</h1>
+			<h2 class="mb-5 text-6xl font-bold">Fube</h2>
+		</div>
+	</div>
+</div>
