197798bfba7049a705784eb9b0016ec74a96a8f0
Fube <fubeitch@gmail.com>
2022-02-18 13:24:40 -0500

    Replaced inline hamburger SVG with iconify Icon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -94,19 +94,7 @@
 						<!-- Hamburger -->
 						<div class="flex-none dropdown block sm:hidden">
 							<button class="btn btn-square btn-ghost" bind:this={hamburger}>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									class="inline-block w-6 h-6 stroke-current"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M4 6h16M4 12h16M4 18h16"
-									/>
-								</svg>
+								<Icon icon="charm:menu-hamburger" width="36" />
 							</button>
 							<ul
 								tabindex="0"
